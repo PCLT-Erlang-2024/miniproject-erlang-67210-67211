bc66c35a182a7b74d825a0e6671d6fa988043787
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2024-11-23 20:45:58 +0000

    cleanup

--- task1.erl
+++ task1.erl
@@ -1,7 +1,6 @@
 -module(task1).
 
--export([main/0, start_conveyor_belts/0, start_generate_products/1, conveyor_belt/2,
-         truck_provider/0, process_product/2]).
+-export([main/0, generate_products/0, conveyor_belt/1, truck_provider/0]).
 
 -define(NUM_PRODUCTS, 20).
 -define(NUM_CONVEYOR_BELTS, 2).
@@ -23,38 +22,36 @@ log_with_time(Format) ->
 get_cb_process_alias(Id) ->
     list_to_atom("cb" ++ integer_to_list(Id)).
 
-start_generate_products(MainPid) ->
-    F = fun() -> generate_products(MainPid) end,
-    spawn(F).
+start_generate_products() ->
+    spawn(?MODULE, generate_products, []).
 
-generate_products(MainPid) ->
-    generate_products(1, MainPid).
+generate_products() ->
+    generate_products(1).
 
-generate_products(Id, MainPid) when Id =< ?NUM_PRODUCTS ->
+generate_products(Id) when Id =< ?NUM_PRODUCTS ->
     Product = create_product(Id),
-    log_with_time("Generate Products: Product ~p with size ~p",
+    log_with_time("Generate Products :: Product ~p with size ~p",
                   [Product#product.id, Product#product.size]),
     CbId = Id rem ?NUM_CONVEYOR_BELTS + 1,
     get_cb_process_alias(CbId) ! {product, Product},
     timer:sleep(500),
-    generate_products(Id + 1, MainPid);
-generate_products(_, MainPid) ->
-    [get_cb_process_alias(CbId) ! {stop, stop} || CbId <- lists:seq(1, ?NUM_CONVEYOR_BELTS)],
-    tp ! {stop, stop},
-    MainPid ! {all_products_generated, ?NUM_PRODUCTS}.
+    generate_products(Id + 1);
+generate_products(_) ->
+    [get_cb_process_alias(CbId) ! stop || CbId <- lists:seq(1, ?NUM_CONVEYOR_BELTS)],
+    tp ! stop.
 
 create_product(Id) ->
     #product{id = Id, size = 3}.
 
 start_conveyor_belts() ->
-    [register(get_cb_process_alias(Id), spawn(?MODULE, conveyor_belt, [Id, self()]))
+    [register(get_cb_process_alias(Id), spawn(?MODULE, conveyor_belt, [Id]))
      || Id <- lists:seq(1, ?NUM_CONVEYOR_BELTS)].
 
-conveyor_belt(Id, MainPid) ->
+conveyor_belt(Id) ->
     Truck = get_truck(Id),
-    conveyor_belt(Id, Truck, MainPid).
+    conveyor_belt(Id, Truck).
 
-conveyor_belt(Id, Truck, MainPid) ->
+conveyor_belt(Id, Truck) ->
     receive
         {product, Product} ->
             UpdatedTruck =
@@ -72,9 +69,9 @@ conveyor_belt(Id, Truck, MainPid) ->
                            Product#product.id,
                            UpdatedTruck#truck.load,
                            UpdatedTruck#truck.capacity]),
-            MainPid ! {product_processed, Id},
-            conveyor_belt(Id, UpdatedTruck, MainPid);
-        {stop, stop} ->
+            main ! {processed, Product#product.id},
+            conveyor_belt(Id, UpdatedTruck);
+        stop ->
             log_with_time("Conveyor Belt ~p :: Stopping", [Id])
     end.
 
@@ -103,35 +100,23 @@ truck_provider(Id) ->
                       capacity = 10,
                       load = 0}},
             truck_provider(Id + 1);
-        {stop, stop} ->
+        stop ->
             log_with_time("Truck Provider :: Stopping")
     end.
 
-process_product(Product, Truck) ->
-    if Truck#truck.load + Product#product.size =< Truck#truck.capacity ->
-           UpdatedTruck = Truck#truck{load = Truck#truck.load + Product#product.size},
-           {ok, UpdatedTruck};
-       true ->
-           RemainingProduct =
-               Product#product{size =
-                                   Product#product.size
-                                   - (Truck#truck.capacity - Truck#truck.load)},
-           {error, RemainingProduct}
+wait_for_completion(0) ->
+    log_with_time("Main :: All products processed");
+wait_for_completion(Count) ->
+    receive
+        {processed, ProductId} ->
+            log_with_time("Main :: Product ~p processed", [ProductId]),
+            wait_for_completion(Count - 1)
     end.
 
 main() ->
     register(main, self()),
     start_truck_provider(),
     start_conveyor_belts(),
-    start_generate_products(self()),
-    wait_for_completion(?NUM_PRODUCTS).
-
-wait_for_completion(0) ->
-    log_with_time("Main :: All products processed");
-wait_for_completion(Count) ->
-    receive
-        {all_products_generated, Total} ->
-            wait_for_completion(Total);
-        {product_processed, _Id} ->
-            wait_for_completion(Count - 1)
-    end.
+    start_generate_products(),
+    wait_for_completion(?NUM_PRODUCTS),
+    unregister(main).
